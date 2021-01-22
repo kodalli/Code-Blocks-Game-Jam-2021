c42fa6b41395eb9c4e73a14afc9d350c00c6697f
vazeraja <68131084+vazeraja@users.noreply.github.com>
2021-01-22 18:54:30 -0500

    --

---
+++ CodeBlocksGameJamUnity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        movement.x = Input
+    }
+    void FixedUpdate()
+    {
+        
+    }
+}
