ec03b841c2c031087452e60993fdf7c8291baa8b
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-01-22 20:55:11 -0500

    running and idle animation and flip sprite to match direction

--- CodeBlocksGameJamUnity/Assets/Scripts/PlayerMovement.cs
+++ CodeBlocksGameJamUnity/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,13 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     Vector2 movement;
+    bool facingRight = false;
+    Animator anim;
 
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
@@ -20,5 +23,20 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        // animate player
+        anim.SetFloat("AnimSpeed", Mathf.Abs(movement.x) + Mathf.Abs(movement.y));
+
+        // make character face correction direction
+        if (movement.x > 0 && !facingRight)
+            Flip();
+        else if (movement.x < 0 && facingRight)
+            Flip();
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
