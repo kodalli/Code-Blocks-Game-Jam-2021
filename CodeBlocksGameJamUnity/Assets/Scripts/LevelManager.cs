fce0344a55ea79d62f46265d7456e2137087286a
vazeraja <68131084+vazeraja@users.noreply.github.com>
2021-01-23 17:53:32 -0500

    main menu and enemy spawner

---
+++ CodeBlocksGameJamUnity/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    public Transform respawnPoint;
+    public GameObject playerPrefab;
+
+    private void Awake()
+    {
+        instance = this;
+        Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+    }
+
+    public void Respawn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ CodeBlocksGameJamUnity/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject[] enemies;
+    private int randX;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            int l = enemies.Length;
+            randX = Random.Range(0, l);
+            Instantiate(enemies[randX], transform.position, Quaternion.identity);
+        }
+    }
+}
