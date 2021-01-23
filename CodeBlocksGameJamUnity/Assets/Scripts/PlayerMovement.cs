844f1bd8a210feed31adef8dbe54fa273e20e164
vazeraja <68131084+vazeraja@users.noreply.github.com>
2021-01-22 19:36:28 -0500

    movement works

--- CodeBlocksGameJamUnity/Assets/Scripts/PlayerMovement.cs
+++ CodeBlocksGameJamUnity/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    Vector2 movement;
 
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
     void FixedUpdate()
     {
-        
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
