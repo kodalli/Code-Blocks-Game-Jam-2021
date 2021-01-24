8cd9c120fbe5007fe260e33dda898ddf69e6df69
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-01-23 22:19:09 -0500

    added ship animated icon for map, fixed screen jitter
    changed camera follow lateupdate to fixedupdate because physics updates independently

--- CodeBlocksGameJamUnity/Assets/Scripts/MainMenu.cs
+++ CodeBlocksGameJamUnity/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Ship Interior 2");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void QuitGame()
     {
