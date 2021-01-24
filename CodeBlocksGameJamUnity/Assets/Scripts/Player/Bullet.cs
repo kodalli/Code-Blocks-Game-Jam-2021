b5cbf6b2594c495319d1774c11cf7220310440d5
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-01-24 15:39:52 -0500

    audioi

--- CodeBlocksGameJamUnity/Assets/Scripts/Player/Bullet.cs
+++ CodeBlocksGameJamUnity/Assets/Scripts/Player/Bullet.cs
@@ -13,7 +13,7 @@ public class Bullet : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Player") || collision.collider.CompareTag("Money"))
+        if (collision.collider.CompareTag("Player"))
             return;
         Die();
     }
